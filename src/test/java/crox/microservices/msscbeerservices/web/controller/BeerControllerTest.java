e2684c2902ea862df6dcdf2e74baafd8f85b01a1
loris croci <loris.croci@openmindonline.it>
2021-11-14 14:03:06 +0100

    created tests for beer controller

---
+++ src/test/java/crox/microservices/msscbeerservices/web/controller/BeerControllerTest.java
@@ -0,0 +1,61 @@
+package crox.microservices.msscbeerservices.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import crox.microservices.msscbeerservices.web.model.BeerDto;
+
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest
+{
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    final String URL = "/api/v1/beer/";
+
+    @Test
+    void getBeerById() throws Exception
+    {
+        mockMvc.perform(get(URL + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() throws Exception
+    {
+        mockMvc.perform(
+                post(URL).accept(MediaType.APPLICATION_JSON).content(beerDtoToJSON(generateBeerDto())))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeer() throws Exception
+    {
+        mockMvc.perform(put(URL + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON).content(beerDtoToJSON(generateBeerDto())))
+                .andExpect(status().isCreated());
+    }
+
+    private BeerDto generateBeerDto()
+    {
+        return BeerDto.builder().build();
+    }
+
+    private String beerDtoToJSON(BeerDto beerDto) throws JsonProcessingException
+    {
+        return objectMapper.writeValueAsString(beerDto);
+    }
+}
