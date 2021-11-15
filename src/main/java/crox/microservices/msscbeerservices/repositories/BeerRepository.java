c94ad65597ed22f61733f2a4c0d2c0a56096f8ba
loris croci <loris.croci@openmindonline.it>
2021-11-15 21:23:54 +0100

    adding beer repository

---
+++ src/main/java/crox/microservices/msscbeerservices/repositories/BeerRepository.java
@@ -0,0 +1,13 @@
+package crox.microservices.msscbeerservices.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import crox.microservices.msscbeerservices.domain.Beer;
+
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>
+{
+
+}
